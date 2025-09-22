580f38d98bd579c3b1fbbff1a2af6907debc1621
eddyittech-ux <eddyittech@gmail.com>
2025-09-22 19:28:20 -0400

    Create juegos.js

---
+++ sections/juegos.js
@@ -0,0 +1,5 @@
+// sections/juegos.js
+(() => {
+  // Dejamos el módulo estable sin llamadas a tablas (para no chocar con RLS de juegos por ahora).
+  window.NT.sections.juegos = { /* placeholder */ };
+})();
